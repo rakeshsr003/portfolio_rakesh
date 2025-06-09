059f1a362969757aac9984a8dd32a9334ef45b04
Rakesh <rakeshsr003@gmail.com>
2025-06-09 13:18:16 +0530

    initial commit

---
+++ Resume/newfile.ts
@@ -0,0 +1,22 @@
+var text= "My name is Rakesh"
+
+var text= "My name is Rakesh"
+const char = text.toLowerCase()
+
+for(let i=char.length -1 ; i>0 ; i--){
+
+console.log(i)
+
+
+}
+
+// inputfield = "//input[@id='']"
+
+// str1 = Rakesh 
+// str2 = Rakesh
+
+// if(str1 == str2)
+
+
+
+
